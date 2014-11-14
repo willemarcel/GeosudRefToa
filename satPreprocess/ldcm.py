6f5cd2410c444a48b97340a83923ec103d994ff2
Wille Marcel <wille@wille.blog.br>
2014-11-14 14:03:45 -0200

    PEP 8 improvements

--- satPreprocess/ldcm.py
+++ satPreprocess/ldcm.py
@@ -21,13 +21,12 @@
 """
 
 # libraries
-
 import sys, numpy, os, time, glob
-from osgeo           import gdal
+from osgeo import gdal
 from osgeo.gdalconst import *
 
-# Class Landsat 8 (LDCM)
 
+# Class Landsat 8 (LDCM)
 class Landsat8:
 
     def __init__(self, metafile):
@@ -40,24 +39,25 @@ class Landsat8:
         metalines = meta.readlines()
         self.root = {}
         for line in metalines:
-            if any(i in line for i in ('REFLECTANCE_MULT_BAND_','REFLECTANCE_ADD_BAND_',
-                                       'K1_CONSTANT_BAND_','K2_CONSTANT_BAND_',
-                                       'SUN_AZIMUTH','SUN_ELEVATION')):
+            if any(i in line for i in ('REFLECTANCE_MULT_BAND_',
+                                        'REFLECTANCE_ADD_BAND_',
+                                        'K1_CONSTANT_BAND_','K2_CONSTANT_BAND_',
+                                        'SUN_AZIMUTH','SUN_ELEVATION')):
                 self.root[line.split('=')[0].strip()] = float(line.split('=')[1].strip())
         metalines = None
         meta.close()
         endTime = time.time()
-        print'parsing duration: %s seconds' %(endTime-startTime)
+        print'parsing duration: %s seconds' % (endTime-startTime)
 
     def getGain(self):
         """
         List of Landsat band-specific multiplicative rescaling factor and
         Band-specific additive rescaling factor
         (coastal/aerosol, blue, green, red, nir, swir1, swir2, pan, cirrus)
-        """        
+        """
         self.gain = [self.root['REFLECTANCE_MULT_BAND_%s' %(i+1)] for i in range(9)]
         self.add = [self.root['REFLECTANCE_ADD_BAND_%s' %(i+1)] for i in range(9)]
-    
+
     def getSolarAngle(self):
         """
         Solar Zenithal and Azimuthal Angles in degrees
@@ -99,7 +99,7 @@ class Landsat8:
             if inDs is None:
                 print 'could not open ' + imgfile
                 sys.exit(1)
-            # image size and tiles 
+            # image size and tiles
             cols  = inDs.RasterXSize
             rows  = inDs.RasterYSize
             bands = inDs.RasterCount
@@ -116,12 +116,13 @@ class Landsat8:
                 maxi = 1000
 
             if outpath:
-                outDs = driver.Create('%s%s' %(os.path.join(outpath,os.path.splitext(os.path.basename(imgfile))[0]),outname),
+                outDs = driver.Create('%s%s' % (os.path.join(outpath, os.path.splitext(os.path.basename(imgfile))[0]), outname),
                                       cols, rows, bands, codage)
             else:
-                outDs = driver.Create('%s%s' %(os.path.splitext(imgfile)[0], outname), cols, rows, bands, codage)
+                outDs = driver.Create('%s%s' % (os.path.splitext(imgfile)[0], outname),
+                                      cols, rows, bands, codage)
             if outDs is None:
-                print 'could not create %s%s' %(os.path.splitext(imgfile)[0], outname)
+                print 'could not create %s%s' % (os.path.splitext(imgfile)[0], outname)
                 sys.exit(1)
 
             outBand = outDs.GetRasterBand(1)
@@ -138,16 +139,17 @@ class Landsat8:
                         numCols = xBSize
                     else:
                         numCols = cols - j
-                    data = canal.ReadAsArray(j,i,numCols, numRows).astype(numpy.float)
+                    data = canal.ReadAsArray(j, i, numCols, numRows).astype(numpy.float)
                     # TOA Reflectance with correction for sun angle
-                    toa = (maxi*(self.gain[band]*data + self.add[band])/
-                           (numpy.cos(numpy.radians(self.solarZAngle)))).astype(nptype) 
+                    toa = (maxi * (self.gain[band] * data + self.add[band]) /
+                           (numpy.cos(numpy.radians(self.solarZAngle)))
+                           ).astype(nptype)
                     # saturated pixels (> 1 or > 1000)
                     mask = numpy.less_equal(toa, maxi)
-                    toa  = numpy.choose(mask, (maxi, toa))
-                    outBand.WriteArray(toa,j,i)   
+                    toa = numpy.choose(mask, (maxi, toa))
+                    outBand.WriteArray(toa, j, i)
             outBand.FlushCache()
-            stats = outBand.GetStatistics(0, 1) 
+            stats = outBand.GetStatistics(0, 1)
             outBand = None
             canal = None
             # projection import
@@ -155,13 +157,8 @@ class Landsat8:
             outDs.SetProjection(inDs.GetProjection())
             # pyramid layers processing
             gdal.SetConfigOption('USE_RRD', 'YES')
-            outDs.BuildOverviews(overviewlist = [2,4,8,16,32,64,128])
-            inDs  = None
+            outDs.BuildOverviews(overviewlist=[2, 4, 8, 16, 32, 64, 128])
+            inDs = None
             outDs = None
         endTime = time.time()
         print 'reflectance processing duration: ' + str(endTime - startTime) + ' seconds'
-
-
-
-
-
